83514a71feae3f010b82a9c48df0e4225d673da5
stephaniesaggal <stersaggal@gmail.com>
2022-08-31 11:03:43 -0300

    adding style

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,13 @@
+import style from './cardapio.module.scss'
+import logo from 'assets/logo.svg';
+
+export default function Cardapio(){
+    return(
+        <main>
+            <nav className={style.menu}>
+                <img src={logo} alt='logo aluroni'></img>
+                dvzsdvzs
+            </nav>
+        </main>
+    )
+}
