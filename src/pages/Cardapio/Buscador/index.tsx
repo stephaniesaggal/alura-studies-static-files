b8862a8e87d64a53cb3aa640500720809b0a2268
stephaniesaggal <stersaggal@gmail.com>
2022-09-01 10:38:26 -0300

    adding style

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import style from './buscador.module.scss'
+import { CgSearch } from 'react-icons/cg';
+
+interface Props{
+    search: string,
+    setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador({search, setSearch}: Props){
+    return(
+        <div className={style.buscador}>
+            <input
+                value={search}
+                onChange={evento => setSearch(evento.target.value)}
+            />
+            <CgSearch 
+                size={20}
+                color="#4C4D5E"
+            />
+        </div>
+    )
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,13 +1,31 @@
 import style from './cardapio.module.scss'
 import logo from 'assets/logo.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
 
 export default function Cardapio(){
+    const [search, setSearch] = useState('');
+
     return(
         <main>
             <nav className={style.menu}>
                 <img src={logo} alt='logo aluroni'></img>
-                dvzsdvzs
             </nav>
+            <header className={style.header}>
+                <div className={style.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
+            <section className={style.cardapio}>
+                <h3 className={style.cardapio__titulo}>Cardápio</h3>
+                <Buscador 
+                search={search} 
+                setSearch={setSearch}
+                />
+                <div className={style.cardapio__filtros}>
+
+                </div>
+            </section>
         </main>
     )
 }
